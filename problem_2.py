cd82e529f0407c3de11b7ae96d4edfa52a9a0e11
bornabhowmik <bornabhowmik4@gmail.com>
2024-08-30 03:37:09 +0600

    all code added

---
+++ problem_2.py
@@ -0,0 +1,7 @@
+# Write a program that will convert celsius value to fahrenheit
+
+temInCelsius = int(input('Enter the celsius velue: '))
+
+temInFahrenheit = (9/5)*temInCelsius + 32
+
+print(temInFahrenheit)
---
+++ problem_4.py
@@ -0,0 +1,9 @@
+# Write a program that will give you the sum of 3 digits
+
+digits1 = int(input('Enter Digits 1: '))
+digits2 = int(input('Enter Digits 2: '))
+digits3 = int(input('Enter Digits 3: '))
+
+sum_of_digits = digits1 + digits2 + digits3
+
+print(sum_of_digits)
