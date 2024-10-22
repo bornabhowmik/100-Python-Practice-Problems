1af40c5b03ceb305a65b697ce8f55ef3b78d9d9a
bornabhowmik <bornabhowmik4@gmail.com>
2024-10-22 21:23:11 +0600

    all code added

---
+++ problem_42.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the number of rows: "))
+
+for i in range(1, 4):
+    # for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+
+    for i in range(2, 0, -1):
+        print("*", end=" ")
+    print()
